813d3e179a15ccbe549f248420933399a6aaf087
TheSchnaz <theschnaz@gmail.com>
2022-11-24 09:45:17 -0500

    we now have an arrary of all song ids

--- src/WebPlayback.jsx
+++ src/WebPlayback.jsx
@@ -1,6 +1,22 @@
 import React, { useState, useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 
 const axios = require("axios").default;
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAOf8EL0mCIS89vymdsRsjq0-KHJdXDTW0",
+  authDomain: "musalives.firebaseapp.com",
+  projectId: "musalives",
+  storageBucket: "musalives.appspot.com",
+  messagingSenderId: "353183638905",
+  appId: "1:353183638905:web:5e3f21c2a9c0bc7978bba8",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
 
 const track = {
   name: "",
@@ -32,8 +48,50 @@ const addToQueue = async () => {
   }
 };
 
+const addLikedTracks = async (token) => {
+  //get all tracks
+  const querySnapshot = await getDocs(collection(db, "songs"));
+  let addTrackIds = [];
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log("doc.data().track.id", " => ", doc.data().track.id);
+    addTrackIds.push(doc.data().track.id);
+  });
+
+  console.log("tracks arrary ", addTrackIds)
+
+  try {
+    const response = await axios.get("https://api.spotify.com/v1/me/tracks", {
+      params: {
+        market: "ES",
+        limit: "10",
+        offset: "5",
+      },
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+    });
+    console.log("liked response", response.data.items);
+
+    try {
+      let i = 0;
+      response.data.items.forEach((item) => {
+        //const docRef = addDoc(collection(db, "songs"), response.data.items[i]);
+        i = i + 1;
+      });
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 function WebPlayback(props) {
-  console.log("WebPlayBack props", props);
+  //console.log("WebPlayBack props", props);
+  console.log("props token", props.token);
 
   const [is_paused, setPaused] = useState(false);
   const [is_active, setActive] = useState(false);
@@ -147,6 +205,15 @@ function WebPlayback(props) {
               }}
             >
               Add songs to queue
+            </button>
+            <br />
+            <button
+              className="btn-spotify"
+              onClick={() => {
+                addLikedTracks(props.token);
+              }}
+            >
+              get liked tracks
             </button>
           </div>
         </div>
